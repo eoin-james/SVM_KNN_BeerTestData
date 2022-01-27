240e492fc39e6b74d37b1a0ebda1f70aff2acb8b
eoin-james <eoin.james@mcallister.ai>
2022-01-27 12:52:35 +0000

    Update to the KNN file

--- KNN.py
+++ KNN.py
@@ -1,13 +1,26 @@
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
-import data_pre_process as pp
+import data_pre_process as dpp
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import pandas as pd
+import itertools
+from sklearn.metrics import confusion_matrix
 
-train_path = 'BeerData/beer_training.csv'
-test_path = 'BeerData/beer_test.csv'
+"""
+K Nearest Neighbour Classifier for Beer Classification
+"""
 
+# Data Path
+TRAIN_PATH = 'BeerData/beer_training.csv'
+TEST_PATH = 'BeerData/beer_test.csv'
+
+# HyperParam
+K = list(range(9, 14, 2))  # K = 9 -> 13 odd only
+P = range(1, 3)  # Manhattan / Euclidean
+WEIGHTS = ['uniform', 'distance']  # Weight function
+
+#
 h = .02  # step size in the mesh
 
 # Create color maps
@@ -15,24 +28,47 @@ cmap_light = ListedColormap(['orange', 'cyan', 'cornflowerblue'])
 cmap_bold = ListedColormap(['darkorange', 'c', 'darkblue'])
 
 
-def knn():
+def knn_hp_testing(x_train, y_train, x_test, y_test):
     y_pred = None
-    x_train, x_test, y_train, y_test, sc = pp.pre_process(train_path, test_path)
+    for k, p, w in itertools.product(K, P, WEIGHTS):
+        classifier = KNeighborsClassifier(
+            n_neighbors=k,
+            weights=w,
+            algorithm='auto',
+            metric='minkowski',
+            p=p
+        )
+        classifier.fit(x_train, y_train.ravel())
 
-    for k in range(1, 16):
-        for p in range(1, 3):
-            for weights in ['uniform', 'distance']:
-                classifier = KNeighborsClassifier(n_neighbors=k, metric='minkowski', p=p, weights=weights)
-                classifier.fit(x_train, y_train.ravel())
+        y_pred = classifier.predict(x_test)
+        match = len([True for i, j in zip(y_pred, y_test) if i == j])
+        actual = len(y_test)
 
-                # Predicting the Test set results
-                y_pred = classifier.predict(x_test)
-                match = len([True for i, j in zip(y_pred, y_test) if i == j])
-                actual = len(y_test)
-                print("k =", k, '; p =', p, '; Weight =', weights, "; Match = ", match)
+        cm = confusion_matrix(y_test, y_pred)
 
-    return y_pred, y_test
+        print('{matches}/{total} matches for K={k}, P={p}, Weight={w}'.format(matches=match, total=actual, k=k, p=p, w=w))
+        print(cm)
+        print()
+
+
+def knn():
+    k, p, w = None, None, None
+    classifier = KNeighborsClassifier(
+        n_neighbors=k,
+        weights=w,
+        algorithm='auto',
+        metric='minkowski',
+        p=p
+    )
+
+def main():
+    # Preprocess Data
+    x_train, x_test, y_train, y_test, sc = dpp.pre_process(TRAIN_PATH, TEST_PATH)
+
+    # Run Classifier
+    knn_hp_testing(x_train, y_train, x_test, y_test)
 
 
 if __name__ == '__main__':
-    predicted, actual = knn()
+    # predicted, actual = knn()
+    main()
